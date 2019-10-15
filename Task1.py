f295deddf5c6db111026c7094b4a6fd5beb04708
Ramiz Raja <informramiz@gmail.com>
2019-10-16 04:10:30 +0500

    feat: Complete task-2

--- Task1.py
+++ Task1.py
@@ -14,7 +14,32 @@ with open('calls.csv', 'r') as f:
 
 """
 TASK 1:
-How many different telephone numbers are there in the records? 
+How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+#A helper function to count numbers for each type of recod
+def count_distinct_numbers_each(records, dictionary = {}):
+    distinct_numbers_count = 0
+    for record in records:
+        #first 2 recorders of either calls or texts are numbers so check each
+        for number in list(record[0:2]):
+            #only count a number if it does not exist already in the dictionary
+            if number not in dictionary:
+                distinct_numbers_count += 1
+                dictionary[number] = True
+    return distinct_numbers_count
+
+# write a function that count the distinct numbers, to avoid code duplication
+def count_distinct_numbers(calls, texts):
+    #a dictionary to mark numbers as counted
+    dictionary = {}
+    #count distinct numbers in calls
+    distinct_numbers_count = count_distinct_numbers_each(calls, dictionary)
+    #count distinct numbers in texts records
+    distinct_numbers_count += count_distinct_numbers_each(texts, dictionary)
+    return distinct_numbers_count
+
+distinct_numbers_count = count_distinct_numbers(calls, texts)
+print("There are %s different telephone numbers in the records." % distinct_numbers_count)
