3feabfa292ee82479f27512e9afb6fd2e29660c9
Ramiz Raja <informramiz@gmail.com>
2019-10-16 03:43:48 +0500

    feat: Complete first task

--- Task0.py
+++ Task0.py
@@ -19,4 +19,5 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-
+print("First recod of texts, %s texts %s at time %s" % (texts[0][0], texts[0][1], texts[0][2]))
+print("First recod of calls, %s calls %s at time %s, lasting %s seconds" % (calls[0][0], calls[0][1], calls[0][2], calls[0][3]))
