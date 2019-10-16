4062d6fa0ac6a235d06902422559c48f6da2a312
Ramiz Raja <informramiz@gmail.com>
2019-10-16 06:42:12 +0500

    feat: Complete Task-4
    This is due to some restrictions

--- Task4.py
+++ Task4.py
@@ -24,4 +24,22 @@ Print a message:
 <list of numbers>
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
+#numbers that received calls
+call_receivers = set([r for _, r, _, _ in calls])
+#numbers that either sent or received a text
+texters = set([s for s, _, _ in texts])
+#merge text senders with receivers as we only need number we are interested in either
+texters.union(set([r for _, r, _ in texts]))
 
+outgoing_only_numbers = set()
+#for all the numbers that made calls but are not in call receiver, text sender or text receiver
+#are the possible telemarketers
+for number, _, _, _ in calls:
+    if number not in call_receivers and number not in texters:
+        outgoing_only_numbers.add(number)
+
+
+telemarketers = sorted(outgoing_only_numbers)
+print("These numbers could be telemarketers: ")
+for number in telemarketers:
+    print(number)
